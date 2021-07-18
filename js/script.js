239daeddd7c3571a3d0f1907f9c7d22dc31e133e
Jorge Obrecht <jorgeobrecht@gmail.com>
2021-07-18 19:35:38 -0300

    Projeto

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const form = document.getElementById('form')
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    let nome=document.getElementById('nome').value;
+    let email=document.getElementById('email').value;
+    let data={
+        nome,
+        email,
+    }
+
+    let convertData=JSON.stringify(data);
+
+    localStorage.setItem('lead',convertData)
+
+    let content=document.getElementById('content')
+
+    let enviado =`<p class="enviado">Informações enviadas</p>` 
+
+    content.innerHTML=enviado
+
+})
